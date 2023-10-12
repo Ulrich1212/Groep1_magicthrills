34599f2437014726faba5bf8dacdd163e3465225
ul212 <2057414@talnet.nl>
2023-10-12 18:47:56 +0200

    test 1

---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="Content">
+    <meta name="keywords" content="Keywords">
+    <meta name="author" content="Ulrich Cruikshank">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Magic Thrills</title>
+    <link rel="stylesheet" href="main.css">
+
+</head>
+<body>
+<div class="container">
+    <div class="image">
+
+    </div>
+    <div class="kan jij de magie aan?">
+        <p class="titel-text">Kan jij de magie aan?</p>
+
+        <div class="kan jij de magie aan?-box">
+            <div class="small-circle"></div>
+            <p class="kan jij de magie aan?-text">lorem ipsum 1</p>
+        </div>
+        <div class="kan jij de magie aan?-box">
+            <div class="small-circle"></div>
+            <p class="kan jij de magie aan?-text">lorem ipsum 2</p>
+        </div>
+        <div class="kan jij de magie aan?-box">
+            <div class="small-circle"></div>
+            <p class="kan jij de magie aan?-text">lorem ipsum 3</p>
+        </div>
+    </div>
+    <h1 id="bekijkattracties-head">avontuurlijke attracties voor jong en oud bekijk onze attracies</h1>
+    <div class="image 2">
+        <h2 id="dag-display">fotos komen hier</h2>
+        <div class="image 2">
+            <div class="knop">
+                <p id="knop-content">*pijl voor foto*</p>
+            </div>
+        </div>
+    </div>
+    <div class="adres">
+        <p id="tip-content">Magic Thrills Spijkweg 30
+            8256 RJ Biddinghuizen</p>
+    </div>
+    <div class="voor wie is magic thrills">
+        <div class="voor wie is magic thrills">
+            <div class="voor wie is magic thrills-circle"></div>
+            <h1 id="voor wie is magic thrills-h1">een fatashtisce dag voor gezinnen en families</h1>
+        </div>
+        <p id="voor wie is magic thrills-content-p">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum est ipsa reprehenderit. Aliquid, animi, beatae
+            consectetur cumque dolore doloribus est eum eveniet ipsa itaque laborum modi nulla, rem sapiente
+            voluptates?</p>
+    </div>
+    <div class="overnchten">
+        <div class="small-circle"></div>
+        <p class="kan jij de magie aan?-text">nog hard gaan? blijf overnachten</p>
+    </div>
+</div>
+</body>
+</html>
