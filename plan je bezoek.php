d1fc6b830898e6ffcd84d986652e5c0c065a1219
ul212 <2057414@talnet.nl>
2023-11-06 13:00:52 +0100

    test 1

---
+++ plan je bezoek.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="Magic Thrills - Fun for Everyone">
+    <meta name="keywords" content="magic, thrills, family, adventure">
+    <meta name="author" content="Ulrich Cruikshank">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Magic Thrills</title>
+    <link rel="stylesheet" type="text/css" href="main.css">
+</head>
+
+<body>
+
+<div class="container">
+    <h1 class="title">Plan je bezoek</h1>
+    <div class="content">
+
+        <div class="feature-box">
+            <img src="img/download.jpg" alt="mascot" width="250" height="200">
+            <p class="feature-text">gezelig een dag uit</p>
+        </div>
+        <div class="feature-box2">
+            <img src="img/4212-1kolomsfoto-Walibi-READY.jpg" alt="achtbaan" width="250" height="200">
+            <p class="feature-text">Fantastische attracties!</p>
+        </div>
+        <div class="feature-box3">
+            <img src="img/kaart.jpg" alt="kaart" width="250" height="200">
+            <p class="feature-text">kaart van walibi</p>
+        </div>
+    </div>
+
+    <h2 class="section-title">Avontuurlijke attracties voor jong en oud. Bekijk onze attracties.</h2>
+
+    <div class="description">
+        <div class="circle"></div>
+        <h2 class="description-title">Een fantastische dag voor gezinnen en families</h2>
+        <p class="description-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum est ipsa reprehenderit. Aliquid, animi, beatae consectetur cumque dolore doloribus est eum eveniet ipsa itaque laborum modi nulla, rem sapiente voluptates?</p>
+    </div>
+    <div class="stay-over">
+        <div class="small-circle"></div>
+        <p class="stay-over-text">Nog zin om door te gaan? Blijf overnachten.</p>
+    </div>
+
+    <!-- Add the new sections here -->
+    <div class="section">
+        <h2>Section 1</h2>
+        <p>This is the content of Section 1. It can contain paragraphs, images, and other elements.</p>
+    </div>
+    <div class="section">
+        <h2>Section 2</h2>
+        <p>This is the content of Section 2. You can have different content here, and it will be enclosed in its own box.</p>
+    </div>
+</div>
+</body>
+</html>
+
